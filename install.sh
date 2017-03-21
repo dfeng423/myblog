e6f540fe90077dba13cf1d1c1f24c40b04be16b5
yash-123 <pandeyyash1616@gmail.com>
2017-03-21 17:08:07 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-echo "INSTALLING PYTHON"
-sudo apt-get install python
-echo "INSTALLING python-pip"
-sudo apt-get install python-pip
-echo "INSTALLING Django"
-sudo pip install Django==1.10.0
-python manage.py makemigrations
-python manage.py migrate
-echo "Successfully Installed myblog."
